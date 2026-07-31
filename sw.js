583f28f05fde895f317428c25c9c82771091666e
josselin-mattont <mattont.josselin51@gmail.com>
2026-07-31 15:13:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'eloquence-v9';
+const FICHIERS = ['./', './index.html', './cartes-a-imprimer.html', './manifest.webmanifest', './icone-192.png', './icone-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => Promise.all(FICHIERS.map(f => c.add(f).catch(() => {})))).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(rep => {
+      if (rep.ok) { const copie = rep.clone(); caches.open(CACHE).then(c => c.put(e.request, copie)); }
+      return rep;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
